e25bba94c1a28944d1355e24326fbc3a357f8847
Thomas Jackson <jacksontj.89@gmail.com>
2018-10-30 15:42:54 -0700

    Add metrics for poolShrinker

--- server.go
+++ server.go
@@ -441,6 +441,9 @@ func (server *mongoServer) pinger(loop bool) {
 }
 
 func (server *mongoServer) poolShrinker() {
+	stats.PoolShrinkerCreated()
+	defer stats.PoolShrinkerExited()
+
 	ticker := time.NewTicker(1 * time.Minute)
 	for _ = range ticker.C {
 		if server.closed {
